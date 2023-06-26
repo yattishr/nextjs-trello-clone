d9fe714074ec91bd725f76b8868e9501ab6aa2ad
yattishr <yattish@gmail.com>
2023-06-26 16:19:07 +0200

    Functionality to drag and drop cards to other columns and update the database.

--- components/Board.tsx
+++ components/Board.tsx
@@ -5,11 +5,14 @@ import { DragDropContext, DropResult, Droppable } from "react-beautiful-dnd";
 import Column from "./Column";
 
 function Board() {
-  const [board, getBoard, setBoardState] = useBoardStore((state) => [
-    state.board,
-    state.getBoard,
-    state.setBoardState,
-  ]);
+  const [board, getBoard, setBoardState, updateTodoInDB] = useBoardStore(
+    (state) => [
+      state.board,
+      state.getBoard,
+      state.setBoardState,
+      state.updateTodoInDB,
+    ]
+  );
 
   // Sonny Saangha useEffect Hooks on YouTube
   useEffect(() => {
@@ -17,22 +20,88 @@ function Board() {
   }, [getBoard]);
 
   const handleOnDragEnd = (result: DropResult) => {
-    const {destination, source, type } = result;
+    const { destination, source, type } = result;
 
     // check if the user dragged a card outside of the board
-    if(!destination) return;
-   
-    // Handle a column drag
-    if(type==="column") {
-      const entries = Array.from(board.columns.entries())
-      const [removed] = entries.splice(source.index, 1) // remove the column (Splice) from one array
-      entries.splice(destination.index, 0, removed)
+    if (!destination) return;
+
+    // Handle Column drag
+    if (type === "column") {
+      const entries = Array.from(board.columns.entries());
+      const [removed] = entries.splice(source.index, 1); // remove the column (Splice) from one array
+      entries.splice(destination.index, 0, removed);
       const rearrangedColumns = new Map(entries);
       setBoardState({
-        ...board, columns: rearrangedColumns
-      })
+        ...board,
+        columns: rearrangedColumns,
+      });
     }
-  }
+
+    // Handle Card drag
+    // This step is needed as indexes are stored as numbers, instead of id's with DND library
+    const columns = Array.from(board.columns);
+    const startColIndex = columns[Number(source.droppableId)];
+    const finishColIndex = columns[Number(destination.droppableId)];
+
+    const startCol: Column = {
+      id: startColIndex[0],
+      todos: startColIndex[1].todos,
+    };
+
+    const finishCol: Column = {
+      id: finishColIndex[0],
+      todos: finishColIndex[1].todos,
+    };
+
+    // if there was no StartCol or FinishCol return
+    if (!startCol || !finishCol) return;
+
+    // if card was dragged & dropped in same location, do nothing & return
+    if (source.index === destination.index && startCol === finishCol) return;
+
+    // next create a new copy of the Array
+    const newTodos = startCol.todos;
+    const [todoMoved] = newTodos.splice(source.index, 1);
+
+    // if user is dragging & dropping a card in the same column
+    if (startCol.id === finishCol.id) {
+      // same column task drag
+      newTodos.splice(destination.index, 0, todoMoved);
+
+      // create a new column object
+      const newCol = {
+        id: startCol.id,
+        todos: newTodos,
+      };
+      const newColumns = new Map(board.columns);
+      newColumns.set(startCol.id, newCol);
+
+      // set the new Board state
+      setBoardState({ ...board, columns: newColumns });
+    } else {
+      // dragging to another column
+      const finishTodos = Array.from(finishCol.todos);
+      finishTodos.splice(destination.index, 0, todoMoved);
+
+      const newColumns = new Map(board.columns);
+      const newCol = {
+        id: startCol.id,
+        todos: newTodos,
+      };
+
+      newColumns.set(startCol.id, newCol);
+      newColumns.set(finishCol.id, {
+        id: finishCol.id,
+        todos: finishTodos,
+      });
+
+      // Update the Board in Db
+      updateTodoInDB(todoMoved, finishCol.id);
+
+      // set the Board state to the new columns
+      setBoardState({ ...board, columns: newColumns });
+    }
+  };
 
   return (
     <DragDropContext onDragEnd={handleOnDragEnd}>
